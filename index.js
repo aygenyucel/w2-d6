4571a22c3c27da624c1b637c3d5047b59ef1d949
Aygen Yucel <aygenyucel@outlook.com>
2022-10-04 15:23:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = "aygen";
+let lastName = "yucel";
+
+let sum = name + " " + lastName;
+
+console.log("hello, my name is", sum);
+
+let age = 24;
+
+let currYear = 2022;
+
+let birthYear = currYear - age;
+
+console.log("My birth year:", birthYear);
